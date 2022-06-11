d33423f9381964f83027821e2996c91f029280de
Aron Hoogeveen <aron.hoogeveen@gmail.com>
2022-06-11 14:13:02 +0200

    Update create_csv for new dataset
    Update the labels converting script for more search images

--- dataset-tools/create_csv.py
+++ dataset-tools/create_csv.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 banner = """
 This script creates the labeling file (csv) that will be used by the DataSet
 class.
@@ -31,41 +32,64 @@ def main():
         print(f'!!! Directory "{args.dir}" does not exist')
         exit(1)
 
+    errors_occurred = False
+
     for path in os.listdir(args.dir):
         full_path = args.dir + '/' + path
         if os.path.isdir(full_path):
             files = os.listdir(full_path)
-            if 'labels-ranges.txt' not in files:
+            if 'labels2img.csv' not in files:
                 continue  # this is not a directory with manual labeled ranges
+            print(f'Trying to process directory "{full_path}"')
 
-            labels = {}
-            try:
-                frame_names = list(map(lambda x: os.path.basename(x),
-                                       glob(full_path + '/frame_*.jpg')))
-                list.sort(frame_names)  # glob does not guarantee ordering
-                for name in frame_names:
-                    labels[name] = 0
+            #
+            # Loop over each of the search images
+            # Create csv file per search image
+            #
+            df_labels_txt = pd.read_csv(full_path + '/labels2img.csv')  # TODO move fixed filenames to file with constants
+            df_labels = df_labels_txt.copy()
 
-                with open(full_path + '/labels-ranges.txt') as fin:
-                    for line in fin:
-                        my_range = line.split('-')
-                        if len(my_range) == 1:
-                            labels[frame_names[int(my_range[0])]] = 1
-                        else:
-                            for i in range(int(my_range[0]), int(my_range[1])+1):
-                                labels[frame_names[i]] = 1
-            except ValueError as ve:
-                print('!!! file "labels-ranges.txt" has an unsupported layout.')
-                print(f'Exception: {ve}')
-                exit(1)
+            for row in df_labels_txt.iloc:
+                labels_file = full_path + '/' + row['labels']
+                if not os.path.isfile(labels_file):
+                    print(f'!!! ERROR file {labels_file} does not exist. Skipping directory {full_path}.')
+                    errors_occurred = True
+                    continue
 
-            # Write the dict to a csv file
+                labels = {}
+                try:
+                    frame_names = list(map(lambda x: os.path.basename(x),
+                                           glob(full_path + '/frame_*.jpg')))
+                    list.sort(frame_names)  # glob does not guarantee ordering
+                    for name in frame_names:
+                        labels[name] = 0
 
-            s = pd.Series(labels, name='label')
-            s.index.name = 'frame'
-            s = s.reset_index()
-            df = pd.DataFrame(s)
-            df.to_csv(full_path + '/labels.csv')
+                    with open(labels_file) as fin:
+                        for line in fin:
+                            my_range = line.split('-')
+                            if len(my_range) == 1:
+                                labels[frame_names[int(my_range[0])]] = 1
+                            else:
+                                for i in range(int(my_range[0]), int(my_range[1])+1):
+                                    labels[frame_names[i]] = 1
+                except ValueError as ve:
+                    print(f'!!! file "{labels_file}" has an error.')
+                    print(f'Exception: {ve}')
+                    errors_occurred = True
+                    continue
+
+                # Write the dict to a csv file
+
+                s = pd.Series(labels, name='label')
+                s.index.name = 'frame'
+                s = s.reset_index()
+                df = pd.DataFrame(s)
+                file_name = os.path.splitext(os.path.basename(labels_file))[0] + '.csv'
+                df.to_csv(full_path + '/' + file_name)
+                print(f'\tProcessed {os.path.basename(full_path) + "/" + row["labels"]}')
+
+    if errors_occurred:
+        print('!!! There were errors. Please check the commandline log.')
 
 
 if __name__ == '__main__':
