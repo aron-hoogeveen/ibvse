3de1fc6f2a42bf2be8ad274f36f43d14abbb3fbb
Matthijs <M.Korevaar-1@student.tudelft.nl>
2022-06-14 22:33:46 +0200

    Possibly method selector data collection
    collects automatic data for amounts of frames and methods

--- nearestneighbor/nn_main_test.py
+++ nearestneighbor/nn_main_test.py
@@ -110,17 +110,6 @@ def nns(frame_features_in, image_features_in, method, k = None, k_percentage = N
     print(f"Search time per query:\t{time_per_query}")
     print(f"&{np.round((build_time+(time_per_query*args.n_queries))*1000,2)}\t&{np.round(build_time*1000,2)}\t&{np.round(time_per_query*1000,2)}\t&{np.round(mAP,2)}\t&{np.round(recall,2)}")
 
-    filename = r".\test_data\15minresults.csv"
-    build_time_ms = build_time*1000
-    time_per_query_ms = time_per_query*1000
-
-    if not os.path.exists(filename):
-        with open(filename, 'w') as f:
-            f.write(f"n_frames, method, total_time1, total_time1000, mAP, recall\n")
-
-    with open(filename, 'a') as f:
-        f.write(f"{n_frames},{method},{build_time_ms+time_per_query_ms},{build_time_ms+time_per_query_ms*1000},{mAP},{recall}\n")
-
     return build_time, time_per_query, total_time, mAP, recall
 
 
